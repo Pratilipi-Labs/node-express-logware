38bda83fa361fb86920b5ea127bb3d7bafececa2
Giridhar <samanth.giridhar@gmail.com>
2020-03-02 17:45:09 +0530

    Fix response structure

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function RequestLogger(level,serviceName){
 
 RequestLogger.prototype.log =  function (req,res,next) {
     res.on('finish', () => {
-        let msg = {level:config.level,responseCode:res.statusCode,requestId:res.getHeader("request-id") ? res.getHeader("request-id") : "",serviceName:config.serviceName};
+        let msg = {level:config.level.toLowerCase(),responseCode:res.statusCode,uri:req.path,requestId:res.getHeader("request-id") ? res.getHeader("request-id") : "",service:config.serviceName};
         if (config.level == "INFO" || (config.level == "ERROR" && res.statusCode >= 400)) {
             console.log(JSON.stringify(msg));
         }
