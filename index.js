031d73f238c691cf36627402f0baa65d2848a08b
Saurabh Thakur <thakursaurabh1998@gmail.com>
2020-03-04 16:40:28 +0530

    Some code refactors

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-let config={level:"INFO",serviceName:""};
+let config = {};
 
-function RequestLogger(level,serviceName){
-    config.level = level;
-    config.serviceName = serviceName;
+/**
+ *
+ * @param {String} [level="INFO"]
+ * @param {Object} [logger=console] Any custom logger instance used in the codebase, by default `console` will be used
+ * @param {String} [serviceName] Default value will be picked up from the `name` key `package.json`
+ */
+function RequestLogger(level, logger, serviceName) {
+  config.level = (level || 'INFO').toLowerCase();
+  config.serviceName = serviceName || require.main.require('./package').name;
+  config.logger = logger || console;
 }
 
-RequestLogger.prototype.log =  function (req,res,next) {
-    res.on('finish', () => {
-        let msg = {level:config.level.toLowerCase(),responseCode:res.statusCode,uri:req.path,requestId:res.getHeader("request-id") ? res.getHeader("request-id") : "",service:config.serviceName};
-        if (config.level == "INFO" || (config.level == "ERROR" && res.statusCode >= 400)) {
-            console.log(JSON.stringify(msg));
+RequestLogger.prototype.log = function(req, res, next) {
+  const { level, serviceName, logger } = config;
+  res.on('finish', () => {
+    const msg = {
+      level,
+      responseCode: res.statusCode,
+      uri: req.path,
+      requestId: res.getHeader('request-id') || null,
+      service: serviceName,
+    };
+    const logLevelMap = {
+      info: () => {
+        logger.log(JSON.stringify(msg));
+      },
+      error: () => {
+        if (res.statusCode >= 400) {
+          logger.log(JSON.stringify(msg));
         }
-    });
-    next()
-}
+      },
+    };
+    logLevelMap[level] && logLevelMap[level]();
+  });
+  next();
+};
 
 module.exports = RequestLogger;
